cf94f1c4a971f0e64324e1cdbe4a4f65ead46a1d
zhangshili <zhangshili@meituan.com>
2021-05-04 21:21:47 +0800

    增加CommonUtil的log输出类

--- lib/RandomWords.dart
+++ lib/RandomWords.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'utils/CommonUtils.dart';
 
 class RandomWordsWidget extends StatefulWidget {
   @override
@@ -12,6 +13,16 @@ class RandomWordsState extends State<RandomWordsWidget> {
   @override
   Widget build(BuildContext context) {
     final words = WordPair.random();
-    return new Text(words.asCamelCase);
+    return new Column(
+      children: [
+        new Text(words.asCamelCase),
+        new TextButton(
+          onPressed: () {
+            LogUtil.v('onPressed >>>>');
+          },
+          child: Text('TextButton click'),
+        )
+      ],
+    );
   }
 }
