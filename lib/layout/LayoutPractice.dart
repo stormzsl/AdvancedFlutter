674fba6795c9ee60d2929d4dacdb2bf377107154
zhangshili <zhangshili@meituan.com>
2021-05-05 13:53:32 +0800

    添加布局练习

--- lib/layout/LayoutPractice.dart
+++ lib/layout/LayoutPractice.dart
@@ -5,14 +5,88 @@ class LayoutWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget image = _buildImage();
-    return image;
+    Widget favoriteRow = _buildFavoriteRow(context);
+    Widget buttonRow = _buildButtonContainer(context);
+    return new Container(
+      child: new Column(
+        children: [image, favoriteRow, buttonRow],
+      ),
+    );
   }
 
   Widget _buildImage() {
     return Image.asset(
       'assets/images/lake.jpeg',
-      scale: 1.0,
-      height: 400.0,
+    );
+  }
+
+  Widget _buildFavoriteRow(BuildContext context) {
+    Widget sectionTitle = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Row(
+        children: [
+          new Expanded(
+              child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start, //左对齐
+            children: [
+              new Container(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  'Oeschinen Lake Campground',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+                ),
+              ),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(color: Colors.grey[500], fontSize: 16.0),
+              )
+            ],
+          )),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text(
+            '41',
+            style: TextStyle(fontSize: 16.0),
+          )
+        ],
+      ),
+    );
+    return sectionTitle;
+  }
+
+  Widget _buildButtonContainer(BuildContext context) {
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly, //行平均分配
+      children: [
+        _buildButtonColumn(context, Icons.call, "Call"),
+        _buildButtonColumn(context, Icons.near_me, "ROUTE"),
+        _buildButtonColumn(context, Icons.share, "SHARE")
+      ],
+    );
+  }
+
+  Column _buildButtonColumn(
+      BuildContext context, IconData iconData, String label) {
+    Color color = Theme.of(context).primaryColor;
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(bottom: 10.0),
+          child: Icon(
+            iconData,
+            color: color,
+          ),
+        ),
+        Text(
+          label,
+          style: TextStyle(
+              color: color, fontSize: 16.0, fontWeight: FontWeight.w500),
+        )
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,14 @@ class MyAppStateWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: '英语生成列表主标题',
-      theme: ThemeData(primaryColor: Colors.white), //更改页面主题
-      home: LayoutWidget(),
-    );
+        title: '英语生成列表主标题',
+        theme: ThemeData(primaryColor: Colors.blue), //更改页面主题
+        home: new Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('flutter advance'),
+          ),
+          body: LayoutWidget(),
+        ));
   }
 }
