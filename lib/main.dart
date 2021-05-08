d01037f55b6ea320351fafb8ff5e3ffcf8e06da1
zhangshili <zhangshili@meituan.com>
2021-05-09 00:22:16 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,12 @@ class MyAppStateWidget extends StatelessWidget {
     return new MaterialApp(
         title: '英语生成列表主标题',
         theme: ThemeData(primaryColor: Colors.red), //更改页面主题
-        initialRoute: '/a',
-        routes: <String, WidgetBuilder>{
-          '/a': (BuildContext context) => new LayoutWidget(),
-          '/b': (BuildContext context) => new RooButtonWidget(title: 'page B'),
-          '/c': (BuildContext context) => new RooButtonWidget(title: 'page C')
-        },
+        // initialRoute: '/a',
+        // routes: <String, WidgetBuilder>{
+        //   '/a': (BuildContext context) => new LayoutWidget(),
+        //   '/b': (BuildContext context) => new RooButtonWidget(title: 'page B'),
+        //   '/c': (BuildContext context) => new RooButtonWidget(title: 'page C')
+        // },
         home: new Scaffold(
           appBar: AppBar(
             centerTitle: true,
