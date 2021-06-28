a08c2e04a0cb9aa239bbe3d9cc9864ec383a29b8
zhangshili <zhangshili@meituan.com>
2021-06-28 15:59:17 +0800

    修改对话框显示逻辑

--- lib/dialog/dialog_normal.dart
+++ lib/dialog/dialog_normal.dart
@@ -6,127 +6,26 @@ import 'dialog_util.dart';
 //todo https://book.flutterchina.club/chapter7/dailog.html#_7-6-1-使用对话框
 class DialogNormalPage extends StatefulWidget {
   const DialogNormalPage({Key key}) : super(key: key);
-
   @override
   _DialogNormalPageState createState() => _DialogNormalPageState();
 }
 
 class _DialogNormalPageState extends State<DialogNormalPage> {
-  Future<bool> showDialog1(BuildContext context) {
-    return showDialog(
-        context: context,
-        barrierDismissible: true,
-        builder: (context) {
-          return AlertDialog(
-            title: Text("标题"),
-            content: Text("this is content"),
-            actions: <Widget>[
-              RaisedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text("取消"),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  Navigator.of(context).pop(true);
-                },
-                child: Text("确认"),
-              )
-            ],
-          );
-        });
-  }
-
-  Future<void> changeLanguage() async {
-    int i = await showDialog<int>(
-        context: context,
-        builder: (BuildContext context) {
-          return SimpleDialog(
-            title: const Text('请选择语言'),
-            children: <Widget>[
-              SimpleDialogOption(
-                onPressed: () {
-                  // 返回1
-                  Navigator.pop(context, 1);
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 6),
-                  child: const Text('中文简体'),
-                ),
-              ),
-              SimpleDialogOption(
-                onPressed: () {
-                  // 返回2
-                  Navigator.pop(context, 2);
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 6),
-                  child: const Text('美国英语'),
-                ),
-              ),
-            ],
-          );
-        });
-
-    if (i != null) {
-      print("选择了：${i == 1 ? "中文简体" : "美国英语"}");
-    }
-  }
-
-  Future<void> showListDialog() async {
-    int index = await showDialog<int>(
-      context: context,
-      builder: (BuildContext context) {
-        var child = Column(
-          children: <Widget>[
-            ListTile(title: Text("请选择")),
-            Expanded(
-                child: ListView.builder(
-              itemCount: 30,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text("$index"),
-                  onTap: () => Navigator.of(context).pop(index),
-                );
-              },
-            )),
-          ],
-        );
-        //使用AlertDialog会报错
-        //return AlertDialog(content: child);
-
-        // return Dialog(child: child);与下面效果相同
-        return UnconstrainedBox(
-          constrainedAxis: Axis.vertical,
-          child: ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 280),
-            child: Material(
-              child: child,
-              type: MaterialType.card,
-            ),
-          ),
-        );
-      },
-    );
-    if (index != null) {
-      print("点击了：$index");
-    }
-  }
-
+   int temp;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("dialog normal collections"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body:
+      Center(
+        child: ListView(
+          // mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             RaisedButton(
               onPressed: () async {
-                final result = await showDialog1(context);
+                final result = await showAlertDialog(context);
                 if (result == null) {
                   print(">>>> click cancel");
                 } else {
@@ -137,43 +36,63 @@ class _DialogNormalPageState extends State<DialogNormalPage> {
             ),
             RaisedButton(
               onPressed: () {
-                changeLanguage();
+                showSimpleDialog(context);
               },
               child: Text("show SimpleDialog"),
             ),
             RaisedButton(
               onPressed: () {
-                showListDialog();
+                showListDialog(context);
               },
               child: Text("showListDialog"),
             ),
             RaisedButton(
               onPressed: () {
-                showCustomDialog<bool>(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Text("提示"),
-                      content: Text("您确定要删除当前文件吗?"),
-                      actions: <Widget>[
-                        FlatButton(
-                          child: Text("取消"),
-                          onPressed: () => Navigator.of(context).pop(),
-                        ),
-                        FlatButton(
-                          child: Text("删除"),
-                          onPressed: () {
-                            // 执行删除操作
-                            Navigator.of(context).pop(true);
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                );
+                showMyCustomDialog(context);
               },
               child: Text("showCustomDialog"),
-            )
+            ),
+            RaisedButton(onPressed: (){
+              showDeleteConfirmDialog3(context);
+            },child: Text("showDeleteConfirmDialog3"),),
+            RaisedButton(onPressed: (){
+              showDeleteConfirmDialog4(context);
+            },child: Text("showDeleteConfirmDialog4"),),
+            RaisedButton(onPressed: (){
+              showDeleteConfirmDialog5(context);
+            },child: Text("showDeleteConfirmDialog5"),),
+            RaisedButton(onPressed: (){
+              showDeleteConfirmDialog6(context);
+            },child: Text("showDeleteConfirmDialog6"),),
+            RaisedButton(onPressed: (){
+              showMyModalBottomSheet(context);
+            },child: Text("showMyModalBottomSheet"),),
+
+
+            RaisedButton(onPressed: (){
+              showLoadingDialog(context);
+            },child: Text("showLoadingDialog"),),
+
+
+            RaisedButton(onPressed: (){
+              showDatePicker1(context);
+            },child: Text("showDatePicker1"),),
+
+
+            RaisedButton(onPressed: (){
+              showDatePicker2(context);
+            },child: Text("showDatePicker2"),),
+
+
+            RaisedButton(onPressed: (){
+              print(">>>>> temp:$temp");
+            },child: Text("print"),),
+            Container(
+              constraints: BoxConstraints(maxWidth: 80),
+              child:Text("如果我们嫌Loading框太宽，想自定义对话框宽度，这时只使用SizedBox或ConstrainedBox是不行的，原因是showDialog中已经给对话框设置了宽度限制，根据我们在第五章“尺寸限制类容器”一节中所述，我们可以使用UnconstrainedBox先抵消showDialog对宽度的限制，然后再使用SizedBox指定宽度，代码如下：",
+              softWrap: true,),
+
+            ),
           ],
         ),
       ),
--- lib/dialog/dialog_util.dart
+++ lib/dialog/dialog_util.dart
@@ -1,44 +1,427 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// todo 自定义对话框:https://book.flutterchina.club/chapter7/dailog.html#_7-6-2-对话框打开动画及遮罩
-Future<T> showCustomDialog<T>({
-  @required BuildContext context,
-  bool barrierDismissible = true,
-  WidgetBuilder builder,
-}) {
-  final ThemeData theme = Theme.of(context, shadowThemeOnly: true);
-  return showGeneralDialog(
+
+import 'custom_dialog.dart';
+import 'dialog_checkbox.dart';
+
+//自定义dialog
+Future<bool> showMyCustomDialog(BuildContext context) {
+  return showCustomDialog<bool>(
     context: context,
-    pageBuilder: (BuildContext buildContext, Animation<double> animation,
-        Animation<double> secondaryAnimation) {
-      final Widget pageChild = Builder(builder: builder);
-      return SafeArea(
-        child: Builder(builder: (BuildContext context) {
-          return theme != null
-              ? Theme(data: theme, child: pageChild)
-              : pageChild;
-        }),
+    builder: (context) {
+      return AlertDialog(
+        title: Text("提示"),
+        content: Text("您确定要删除当前文件吗?"),
+        actions: <Widget>[
+          FlatButton(
+            child: Text("取消"),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          FlatButton(
+            child: Text("删除"),
+            onPressed: () {
+              // 执行删除操作
+              Navigator.of(context).pop(true);
+            },
+          ),
+        ],
       );
     },
-    barrierDismissible: barrierDismissible,
-    barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
-    barrierColor: Colors.black87, // 自定义遮罩颜色
-    transitionDuration: const Duration(milliseconds: 150),
-    transitionBuilder: _buildMaterialDialogTransitions,
   );
 }
 
-Widget _buildMaterialDialogTransitions(
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-    Widget child) {
-  // 使用缩放动画
-  return ScaleTransition(
-    scale: CurvedAnimation(
-      parent: animation,
-      curve: Curves.easeOut,
-    ),
-    child: child,
+//显示AlertDialog
+Future<bool> showAlertDialog(BuildContext context) {
+  return showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("标题"),
+          content: Text("this is content"),
+          actions: <Widget>[
+            RaisedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("取消"),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: Text("确认"),
+            )
+          ],
+        );
+      });
+}
+
+//显示simpleDialog
+Future<void> showSimpleDialog(BuildContext context) async {
+  int i = await showDialog<int>(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: const Text('请选择语言'),
+          children: <Widget>[
+            SimpleDialogOption(
+              onPressed: () {
+                // 返回1
+                Navigator.pop(context, 1);
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 6),
+                child: const Text('中文简体'),
+              ),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                // 返回2
+                Navigator.pop(context, 2);
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 6),
+                child: const Text('美国英语'),
+              ),
+            ),
+          ],
+        );
+      });
+
+  if (i != null) {
+    print("选择了：${i == 1 ? "中文简体" : "美国英语"}");
+  }
+}
+
+//显示ListDialog
+Future<void> showListDialog(BuildContext context) async {
+  int index = await showDialog<int>(
+    context: context,
+    builder: (BuildContext context) {
+      var child = Column(
+        children: <Widget>[
+          ListTile(title: Text("请选择")),
+          Expanded(
+              child: ListView.builder(
+            itemCount: 30,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text("$index"),
+                onTap: () => Navigator.of(context).pop(index),
+              );
+            },
+          )),
+        ],
+      );
+      //使用AlertDialog会报错
+      //return AlertDialog(content: child);
+
+      // return Dialog(child: child);与下面效果相同
+      return UnconstrainedBox(
+        constrainedAxis: Axis.vertical,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 280),
+          child: Material(
+            child: child,
+            type: MaterialType.card,
+          ),
+        ),
+      );
+    },
+  );
+  if (index != null) {
+    print("点击了：$index");
+  }
+}
+
+//显示有状态组件的Dialog
+Future<bool> showDeleteConfirmDialog3(BuildContext context) {
+  bool _withTree = false; //记录复选框是否选中
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text("提示"),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("您确定要删除当前文件吗?"),
+            Row(
+              children: <Widget>[
+                Text("同时删除子目录？"),
+                DialogCheckbox(
+                  value: _withTree, //默认不选中
+                  onChanged: (bool value) {
+                    //更新选中状态
+                    _withTree = value;
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text("取消"),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          FlatButton(
+            child: Text("删除"),
+            onPressed: () {
+              // 将选中状态返回
+              Navigator.of(context).pop(_withTree);
+            },
+          ),
+        ],
+      );
+    },
   );
 }
+
+//显示有状态组件的Dialog,不同于showDeleteConfirmDialog3的实现方式
+Future<bool> showDeleteConfirmDialog4(BuildContext context) {
+  bool _withTree = false; //记录复选框是否选中
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text("提示"),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("您确定要删除当前文件吗?"),
+            Row(
+              children: <Widget>[
+                Text("同时删除子目录？"),
+                StatefulBuilder(builder: (context, _setState) {
+                  return Checkbox(
+                      value: _withTree,
+                      onChanged: (bool value) {
+                        //_setState方法实际就是该StatefulWidget的setState方法，
+                        //调用后builder方法会重新被调用
+                        _setState(() {
+                          //更新选中状态
+                          _withTree = value;
+                        });
+                      });
+                }),
+              ],
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text("取消"),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          FlatButton(
+            child: Text("删除"),
+            onPressed: () {
+              // 将选中状态返回
+              Navigator.of(context).pop(_withTree);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+//传递context所需
+class StatefulBuilder extends StatefulWidget {
+  const StatefulBuilder({
+    Key key,
+    @required this.builder,
+  })  : assert(builder != null),
+        super(key: key);
+
+  final StatefulWidgetBuilder builder;
+
+  @override
+  _StatefulBuilderState createState() => _StatefulBuilderState();
+}
+
+class _StatefulBuilderState extends State<StatefulBuilder> {
+  @override
+  Widget build(BuildContext context) => widget.builder(context, setState);
+}
+
+//todo 参考了setState的原理实现方式，具体详情参考：https://book.flutterchina.club/chapter7/dailog.html#_7-6-4-对话框状态管理
+Future<bool> showDeleteConfirmDialog5(BuildContext context) {
+  bool _withTree = false;
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text("提示"),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("您确定要删除当前文件吗?"),
+            Row(
+              children: <Widget>[
+                Text("同时删除子目录？"),
+                Checkbox(
+                  // 依然使用Checkbox组件
+                  value: _withTree,
+                  onChanged: (bool value) {
+                    // 此时context为对话框UI的根Element，我们
+                    // 直接将对话框UI对应的Element标记为dirty
+                    (context as Element).markNeedsBuild();
+                    _withTree = !_withTree;
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text("取消"),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          FlatButton(
+            child: Text("删除"),
+            onPressed: () {
+              // 执行删除操作
+              Navigator.of(context).pop(_withTree);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+//基于showDeleteConfirmDialog5实现方式的优化,最优解
+Future<bool> showDeleteConfirmDialog6(BuildContext context) {
+  bool _withTree = false;
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text("提示"),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("您确定要删除当前文件吗?"),
+            Row(
+              children: <Widget>[
+                Text("同时删除子目录？"),
+                Builder(builder: (BuildContext context) {
+                  return Checkbox(
+                    // 依然使用Checkbox组件
+                    value: _withTree,
+                    onChanged: (bool value) {
+                      // 此时context为对话框UI的根Element，我们
+                      // 直接将对话框UI对应的Element标记为dirty
+                      (context as Element).markNeedsBuild();
+                      _withTree = !_withTree;
+                    },
+                  );
+                }),
+              ],
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text("取消"),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          FlatButton(
+            child: Text("删除"),
+            onPressed: () {
+              // 执行删除操作
+              Navigator.of(context).pop(_withTree);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+// 弹出底部菜单列表模态对话框
+Future<int> showMyModalBottomSheet(BuildContext context) {
+  return showModalBottomSheet<int>(
+    context: context,
+    builder: (BuildContext context) {
+      return ListView.builder(
+        itemCount: 30,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text("$index"),
+            onTap: () {
+              Navigator.of(context).pop(index);
+            },
+          );
+        },
+      );
+    },
+  );
+}
+
+//todo https://book.flutterchina.club/chapter7/dailog.html#_7-6-5-其它类型的对话框 显示Loading框
+showLoadingDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false, //点击遮罩不关闭对话框
+    builder: (context) {
+      return AlertDialog(
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            CircularProgressIndicator(),
+            Padding(
+              padding: const EdgeInsets.only(top: 26.0),
+              child: Text("正在加载，请稍后..."),
+            )
+          ],
+        ),
+      );
+    },
+  );
+}
+
+//日历相关 todo https://book.flutterchina.club/chapter7/dailog.html#_7-6-5-其它类型的对话框
+Future<DateTime> showDatePicker1(BuildContext context) {
+  var date = DateTime.now();
+  return showDatePicker(
+    context: context,
+    initialDate: date,
+    firstDate: date,
+    lastDate: date.add( //未来30天可选
+      Duration(days: 30),
+    ),
+  );
+}
+
+Future<DateTime> showDatePicker2(BuildContext context) {
+  var date = DateTime.now();
+  return showCupertinoModalPopup(
+    context: context,
+    builder: (ctx) {
+      return SizedBox(
+        height: 200,
+        child: CupertinoDatePicker(
+          mode: CupertinoDatePickerMode.dateAndTime,
+          minimumDate: date,
+          maximumDate: date.add(
+            Duration(days: 30),
+          ),
+          maximumYear: date.year + 1,
+          onDateTimeChanged: (DateTime value) {
+            print(value);
+          },
+        ),
+      );
+    },
+  );
+}
+
